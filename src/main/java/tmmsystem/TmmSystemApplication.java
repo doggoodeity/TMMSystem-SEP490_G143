96a4842a3360eb86ffc773c6dfd9a87b6c2f5fa5
Nguyễn Anh Hùng K18 HL <hungnahe180711@fpt.edu.vn>
2025-09-27 23:16:45 +0700

    fix cross origin for front end connect api

--- src/main/java/tmmsystem/TmmSystemApplication.java
+++ src/main/java/tmmsystem/TmmSystemApplication.java
@@ -2,7 +2,10 @@ package tmmsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+@CrossOrigin(origins = "*")
 @SpringBootApplication
 public class TmmSystemApplication {
 
